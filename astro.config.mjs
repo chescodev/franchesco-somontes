66cc062948db51e11f05c91b8975ad1b68f76de3
chescodev <franchesco1510@gmail.com>
2024-07-13 18:40:15 -0500

    Chore :heavy_plus_sign: add Vercel Analytics dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+})
