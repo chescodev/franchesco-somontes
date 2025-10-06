9b8a72845773d1b6a87918251331056545b96905
chescodev <franchesco1510@gmail.com>
2025-10-05 20:50:43 -0500

    update components

--- src/components/utils/Icons.ts
+++ src/components/utils/Icons.ts
@@ -14,6 +14,7 @@ import SqlServerIcon from '@/components/icons/SqlServerIcon.astro'
 import TailwindIcon from '@/components/icons/TailwindIcon.astro'
 import TursoIcon from '@/components/icons/TursoIcon.astro'
 import TypeScriptIcon from '@/components/icons/TypeScriptIcon.astro'
+import KotlinIcon from '../icons/KotlinIcon.astro'
 
 export const skillIcons = [
   {
@@ -65,20 +66,20 @@ export const skillIcons = [
     border: 'border-[#06b6d4]',
     bg: 'bg-[#06b6d4]/20',
   },
-  {
-    title: 'Java',
-    image: JavaIcon,
-    alt: 'Java icon',
-    border: 'border-[#bbb5cd]',
-    bg: 'bg-[#bbb5cd]/20',
-  },
-  {
-    title: 'Spring',
-    image: SpringIcon,
-    alt: 'Spring icon',
-    border: 'border-[#6db33f]',
-    bg: 'bg-[#6db33f]/20',
-  },
+  // {
+  //   title: 'Java',
+  //   image: JavaIcon,
+  //   alt: 'Java icon',
+  //   border: 'border-[#bbb5cd]',
+  //   bg: 'bg-[#bbb5cd]/20',
+  // },
+  // {
+  //   title: 'Spring',
+  //   image: SpringIcon,
+  //   alt: 'Spring icon',
+  //   border: 'border-[#6db33f]',
+  //   bg: 'bg-[#6db33f]/20',
+  // },
   {
     title: 'C#',
     image: CSharpIcon,
@@ -87,19 +88,26 @@ export const skillIcons = [
     bg: 'bg-[#7e61d9]/20',
   },
   {
-    title: 'PHP',
-    image: PhpIcon,
-    alt: 'PHP icon',
-    border: 'border-[#777bb4]',
-    bg: 'bg-[#777bb4]/20',
+    title: 'Kotlin',
+    image: KotlinIcon,
+    alt: 'Kotlin icon',
+    border: 'border-[#7F52FF]',
+    bg: 'bg-[#7F52FF]/20',
   },
-  {
-    title: 'MySQL',
-    image: MySqlIcon,
-    alt: 'MySQL icon',
-    border: 'border-[#4479a1]',
-    bg: 'bg-[#4479a1]/20',
-  },
+  // {
+  //   title: 'PHP',
+  //   image: PhpIcon,
+  //   alt: 'PHP icon',
+  //   border: 'border-[#777bb4]',
+  //   bg: 'bg-[#777bb4]/20',
+  // },
+  // {
+  //   title: 'MySQL',
+  //   image: MySqlIcon,
+  //   alt: 'MySQL icon',
+  //   border: 'border-[#4479a1]',
+  //   bg: 'bg-[#4479a1]/20',
+  // },
   {
     title: 'SQL Server',
     image: SqlServerIcon,
@@ -107,19 +115,19 @@ export const skillIcons = [
     border: 'border-[#bbb5cd]',
     bg: 'bg-[#bbb5cd]/20',
   },
-  {
-    title: 'MongoDB',
-    image: MongoDbIcon,
-    alt: 'MongoDB icon',
-    border: 'border-[#47a248]',
-    bg: 'bg-[#47a248]/20',
-  },
+  // {
+  //   title: 'MongoDB',
+  //   image: MongoDbIcon,
+  //   alt: 'MongoDB icon',
+  //   border: 'border-[#47a248]',
+  //   bg: 'bg-[#47a248]/20',
+  // },
   {
     title: 'SQLite',
     image: SqlLiteIcon,
     alt: 'SQLite icon',
-    border: 'border-[#fffff]',
-    bg: 'bg-[#ffff]/20',
+    border: 'border-[#33B5C9]',
+    bg: 'bg-[#33B5C9]/20',
   },
   {
     title: 'Turso',
